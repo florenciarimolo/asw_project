e4bcc15f06de28067b1a9c72ceddd0f35effb7d4
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-11-05 18:50:10 +0100

    issues migration

---
+++ db/migrate/20171105103426_create_issues.rb
@@ -0,0 +1,15 @@
+class CreateIssues < ActiveRecord::Migration[5.1]
+  def change
+    create_table :issues do |t|
+      t.string :title
+      t.string :kind
+      t.string :priority
+      t.string :status
+      t.integer :votes
+      t.string :assignee
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end

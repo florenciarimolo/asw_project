5baee019d4b38c2f6ac3ce1d07cfc6dfc49ee214
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-11-09 21:32:43 +0100

    redirect to root page when user authorizes application

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,3 +2,13 @@ class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
     validates :email, presence: true, uniqueness: true
 end
+
+class << self
+def from_omniauth(auth_hash)
+    user = find_or_create_by(username: auth_hash['info']['nickname'])
+    user.fullname = auth_hash['info']['name']
+    user.image_url = auth_hash['info']['image']
+    user.save!
+    user
+end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109182606) do
+ActiveRecord::Schema.define(version: 20171109190528) do
 
   create_table "issues", force: :cascade do |t|
     t.string "title"
@@ -30,7 +30,9 @@ ActiveRecord::Schema.define(version: 20171109182606) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.index ["username"], name: "index_users_on_username"
     t.index ["username"], name: "sqlite_autoindex_users_1", unique: true
+    t.index [nil], name: "index_users_on_id"
   end
 
 end

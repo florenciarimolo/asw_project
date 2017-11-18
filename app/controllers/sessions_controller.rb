7dc1b44a4a213fe1ba2598bed1982706d0d5b4f7
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-11-18 13:54:35 +0100

    fix syntax error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
-    def create
-        begin
-            @user = User.from_omniauth(request.env['omniauth.auth'])
-            session[:user_username] = @user.username
-            flash[:success] = "Welcome, #{@user.full_name}!"
-            rescue
-            flash[:warning] = "There was an error while trying to authenticate you..."
-        end
-        redirect_to root_path
+  def create
+    begin
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      session[:user_username] = @user.username
+      flash[:success] = "Welcome, #{@user.full_name}!"
+    rescue
+      flash[:warning] = "There was an error while trying to authenticate you..."
+    end
+    redirect_to root_path
+  end
 end

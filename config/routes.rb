6db7c4de551317c3b846433f8aa7621c1b9b9c2a
Jessica Sobreviela Periche <jessica.sobreviela@est.fib.upc.edu>
2017-11-19 00:10:01 +0100

    routes afegides, boto edit comment falla

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  #get /comments/index =>  /comments?issue_id=:issue_id
-  #get /comments/show => /comments/1?issue_id=:issue_id
-  #get /comments/new => /comments/new?issue_id=:issue_id
-  #get /comments/edit => /comments/1/edit?issue_id=:issue_id
-  #post /comments/create => /comments?issue_id=:issue_id
+  get 'comments?issue_id=:issue_id' => 'comments#index'
+  get 'comments/1?issue_id=:issue_id' => 'comments#show'
+  get 'comments/new?issue_id=:issue_id' => 'comments#new' 
+  get 'comments/1/edit?issue_id=:issue_id' => 'comments#edit'
+  post 'comments?issue_id=:issue_id' => 'comments#create'
   resources :comments
   resources :issues
   resources :users

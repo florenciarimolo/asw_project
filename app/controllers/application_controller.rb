6694a48d225894d8a2982a4990e4c1a36b86a754
Florencia Rímolo Figueira <florencia.rimolo@est.fib.upc.edu>
2017-11-19 14:16:24 +0100

    add username cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   def current_user
-    @current_user ||= User.find_by(username: session[:user_username])
+    @current_user ||= User.find_by(username: cookies.encrypted[:user_username])
   end
 
   helper_method :current_user

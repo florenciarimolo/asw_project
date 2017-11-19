9f85a3e66df0e43deb4a6bc3631f3c6b645f757d
Jessica Sobreviela Periche <jessica.sobreviela@est.fib.upc.edu>
2017-11-19 16:55:08 +0100

    rutes eliminades

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'comments?issue_id=:issue_id' => 'comments#index'
-  get 'comments/:id?issue_id=:issue_id' => 'comments#show'
-  get 'comments/new?issue_id=:issue_id' => 'comments#new' 
-  get 'comments/:id/edit?issue_id=:issue_id' => 'comments#edit'
-  post 'comments?issue_id=:issue_id' => 'comments#create'
   resources :comments
   resources :issues
   resources :users
